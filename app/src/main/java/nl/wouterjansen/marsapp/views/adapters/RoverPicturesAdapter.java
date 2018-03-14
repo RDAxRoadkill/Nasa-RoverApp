a596949fe8186ef15be76485fa4eafb704105178
Wouter <wouter97@planet.nl>
2018-03-14 16:52:19 +0100

    Added Picasso package

--- app/src/main/java/nl/wouterjansen/marsapp/views/adapters/RoverPicturesAdapter.java
+++ app/src/main/java/nl/wouterjansen/marsapp/views/adapters/RoverPicturesAdapter.java
@@ -1,14 +1,24 @@
 package nl.wouterjansen.marsapp.views.adapters;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
+import nl.wouterjansen.marsapp.MainActivity;
 import nl.wouterjansen.marsapp.R;
+import nl.wouterjansen.marsapp.views.RoverDetail;
 
 /**
  * Created by Wouter on 3/13/2018.
@@ -16,17 +26,17 @@ import nl.wouterjansen.marsapp.R;
 
 public class RoverPicturesAdapter extends ArrayAdapter<String> {
     private Activity context;
-    private String[] ids;
-    private String[] img_srcs;
-    private String[] camera_full_names;
+    private String[] idArray;
+    private String[] img_srcArray;
+    private String[] full_nameArray;
 
-    public RoverPicturesAdapter(Activity context, String[] ids, String[] img_srcs, String[] camera_full_names) {
-        super(context, R.layout.rover_row, ids);
+    public RoverPicturesAdapter(Activity context, String[] idArray, String[] img_srcArray, String[] full_nameArray) {
+        super(context, R.layout.rover_row, idArray);
         this.context = context;
 
-        this.ids = ids;
-        this.img_srcs = img_srcs;
-        this.camera_full_names = camera_full_names;
+        this.idArray = idArray;
+        this.img_srcArray = img_srcArray;
+        this.full_nameArray = full_nameArray;
     }
 
     @Override
@@ -37,12 +47,14 @@ public class RoverPicturesAdapter extends ArrayAdapter<String> {
         //Reference to data
         String singlePhoto = getItem(position);
         //Reference to view text
-        TextView photoID = (TextView) roverPictures.findViewById(R.id.pictureID);
+        TextView ID = (TextView) roverPictures.findViewById(R.id.pictureID);
 
         //Reference to image
-        //ImageView mealImage = (ImageView) mealView.findViewById(R.id.mealImage);
+        ImageView photo = (ImageView) roverPictures.findViewById(R.id.marsImage);
 
-        photoID.setText(singlePhoto);
+        ID.setText(singlePhoto);
+        Log.d("IMAGE", img_srcArray.toString());
+        //photo.setImageURI(Uri.parse("https://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG"));
 
         return roverPictures;
     }
